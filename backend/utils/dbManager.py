72f48550dba6807ef5084a8508b85657ca37f5a5
kevathin <Kbert395@mtroyal.ca>
2026-02-14 16:27:58 -0700

    reorganized data folder structure and started dbManager

---
+++ backend/utils/dbManager.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+# tName: table name
+# tType: software name
+# tHeaders: table headers
+# tData: table data
+# tHeadersType: table headers data type
+def createNewDB(tName, tType, tHeaders, tHeadersType, tdata):
+    print("hello world")
